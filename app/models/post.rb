6f9fd35744a13a9fee42153caf5eaef1ad87e52e
omar3o3 <otantawy53@gmail.com>
2022-06-29 19:00:11 -0400

    commit tree 7ad501106f2e56c826fae6ff2891b56825eaf387

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    
+    validates :category, presence: true, 
+        inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+    validates :content, presence: true, 
+        length: { minimum: 100 }
 end
